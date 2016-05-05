ce0c2fd16433be136ae208b265d237b6c8f8e2c4
joses97 <jrs0925@gmail.com>
2016-05-04 21:41:11 -0700

    Signed-off-by: joses97 <jrs0925@gmail.com>

--- Proj/projectLehrv4/proj.h
+++ Proj/projectLehrv4/proj.h
@@ -1,21 +1,28 @@
 /* 
- * File:   proj.h
- * Author: Jose
- *
- * Created on May 3, 2016, 9:42 PM
+ * File:   main.cpp
+ * Author: Jose Sanchez
+ * Purpose: Battleship
+ * Created on 5/3/16
  */
+
+
+//Global constant
 const int SIZE=10;
+
+//Libraries
 #include <string>
 using namespace std;
 
+//header file
 #ifndef PROJ_H
 #define PROJ_H
 
+//Structure 
 struct players{
     string name; //name of player
     char board[SIZE][SIZE]; //board of player
 };
 
-
+//end
 #endif /* PROJ_H */
 
